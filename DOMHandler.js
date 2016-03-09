54d01871e8ffc9a7d7e15ffdcdf0b3f6bc32901c
Bernard Anderson <bernard.anderson@gmail.com>
2016-03-08 20:44:53 -0600

    added the ability to add/remove meats from the total cost and updates the DOM

---
+++ DOMHandler.js
@@ -0,0 +1,24 @@
+// The location to send the total price into the DOM
+var totalPriceTarget = document.getElementById("price-target");
+
+// Variable to hold the final price. Default to 0.
+var finalSandwichPrice = 0;
+
+// Get references to the div elements that has all the topping options
+var meatChooser = document.getElementById("meats");
+
+
+function addToDOM(sentTotalPrice) {
+  // Add the topping to the SandwichMaker to increase the total price
+  finalSandwichPrice = sentTotalPrice;
+  totalPriceTarget.textContent = finalSandwichPrice;
+}
+
+// Listener Events
+meatChooser.addEventListener("change", function(changeEvent) {
+
+  var currentTotalPrice = SandwichMaker.addTopping('addMeat', changeEvent);
+
+  addToDOM(currentTotalPrice);
+  
+});
